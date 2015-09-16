8777075b8db6307af44b50b821144cf0d7e3c566
Jas <41971x@gmail.com>
2015-09-16 15:37:11 -0700

    wrote functions that take in joystick input and compare to deadzone

--- src/Drive/Drive.cpp
+++ src/Drive/Drive.cpp
@@ -16,17 +16,32 @@ Drive::~Drive()
 
 float Drive::setForward(float forward)
 {
-
+	if(forward > DEADZONE || forward < -DEADZONE)
+		return forward;
+	else if(forward < DEADZONE && forward > -DEADZONE)
+		return 0;
+	else
+		return 0;
 }
 
 float Drive::setTurn(float turn)
 {
-
+	if(turn > DEADZONE || turn < -DEADZONE)
+		return turn;
+	else if(turn < DEADZONE && turn > -DEADZONE)
+		return 0;
+	else
+		return 0;
 }
 
 float Drive::setStrafe(float strafe)
 {
-
+	if(strafe > DEADZONE || strafe < -DEADZONE)
+		return strafe;
+	else if(strafe < DEADZONE && strafe > -DEADZONE)
+		return 0;
+	else
+		return 0;
 }
 
 float Drive::linearizeDrive(float inputVal)
